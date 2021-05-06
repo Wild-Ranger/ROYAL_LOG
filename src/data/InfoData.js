8820fe1bd60eeaf29a963cf99eea4aea922c82eb
Wild-Ranger <n.ravivarma846@gmail.com>
2021-05-06 11:55:43 +0530

    interior website

---
+++ src/data/InfoData.js
@@ -0,0 +1,26 @@
+import ImageOne from "../images/interior-1.jpg";
+import ImageTwo from "../images/house-5.jpg";
+
+export const InfoData = {
+  heading: "Explore our beautiful homes",
+  paragraphOne:
+    "Your living room interior sets the tone for the rest of the house and expresses your design aesthetic.",
+  paragraphTwo:
+    "Beautiful Homes Service makes sure you get the look just right, with personalised furniture, tasteful furnishings and rugs, elegant lighting fixtures and well-curated art for your walls.",
+  buttonLabel: "View Homes",
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: "Modern Designs",
+  paragraphOne:
+    "When it comes to modern living spaces, removing things doesn’t have to mean removing comfort.",
+  paragraphTwo:
+    "This minimalist apartment shows how combination of monochromatic color palette and “less is more” principle results in interior that is beautiful to look at but also fun to live in.",
+  buttonLabel: "View Homes",
+  image: ImageTwo,
+  reverse: true,
+  delay: 300,
+};
---
+++ src/data/SliderData.js
@@ -0,0 +1,43 @@
+import ImageOne from '../images/house-1.jpg';
+import ImageTwo from '../images/house-2.jpg';
+import ImageThree from '../images/house-3.jpg';
+import ImageFour from '../images/house-4.jpg';
+
+
+export const SliderData = [
+    {
+        title: 'Luxury Villa in vizag, India',
+        price: '$4,280,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageOne,
+        alt: 'House'
+    },
+
+    {
+        title: 'House in Down st, London',
+        price: '$2,860,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageTwo,
+        alt: 'House'
+    },
+
+    {
+        title: 'House in Mumbai, India',
+        price: '$8,420,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageThree,
+        alt: 'House'
+    },
+
+    {
+        title: 'House in Houston, Texas',
+        price: '$6,660,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageFour,
+        alt: 'House'
+    },
+];
